129d5bbad0c6e1c6b3c2ba4658342b49a6acd77f
rishi0910 <rishimifan45@gmail.com>
2026-07-05 23:14:36 +0530

    refactor: permanently purge Week1 path and enforce Week2 layout on master

--- Week1_Spring_Core_and_Maven/Spring_Core_Exercises/Exercise_1_Configuring_a_Basic_Spring_Application/Code/src/main/java/com/library/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.repository;
-
-public class BookRepository {
-    public void connection() {
-        System.out.println("BookRepository Bean successfully loaded");
-    }
-}
--- Week1_Spring_Core_and_Maven/Spring_Core_Exercises/Exercise_1_Configuring_a_Basic_Spring_Application/Code/src/main/java/com/library/service/BookService.java
+++
@@ -1,7 +0,0 @@
-package com.library.service;
-
-public class BookService {
-    public void log() {
-        System.out.println("BookService Bean successfully loaded ");
-    }
-}
