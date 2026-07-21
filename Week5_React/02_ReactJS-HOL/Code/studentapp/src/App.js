3765099527c2257251ee361eec54c78cb928abce
rishi0910 <rishimifan45@gmail.com>
2026-07-22 03:25:36 +0530

    feat: complete studentapp hands-on 02 for week 5 react

---
+++ Week5_React/02_ReactJS-HOL/Code/studentapp/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import './App.css'; 
+import Home from './Home'; 
+import About from './About'; 
+import Contact from './Contact'; 
+function App() { 
+  return ( 
+    <div className="App"> 
+      <Home /> 
+      <About /> 
+      <Contact /> 
+    </div> 
+  ); 
+} 
+export default App; 
---
+++ Week5_React/02_ReactJS-HOL/Code/studentapp/src/Home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'; 
+export class Home extends Component { 
+  render() { 
+    return ( 
+      <div><h3>Welcome to the Home page of Student Management Portal</h3></div> 
+    ); 
+  } 
+} 
+export default Home; 
